ef79e2ab0d20ca9d116ab16cf2d8977a70717947
Akash Mitra <am.akashmitra1000@gmail.com>
2024-04-12 01:04:22 +0530

    added function to update user pr counts

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use serde::Deserialize;
-use reqwest::{header, Error};
+use reqwest::Error;
 use regex::Regex;
 use dotenv::dotenv;
-use std::env;
+use std::{collections::HashMap, env};
+use chrono::DateTime;
 
 const PER_PAGE: u32 = 50;
 
 #[derive(Deserialize, Debug)]
 struct User{
     login: Option<String>,
-    id: Option<u32>
+    id: Option<u32>,
+    open_pr_count: Option<u32>,
+    merged_pr_count: Option<u32>
 }
 
 #[derive(Deserialize, Debug)]
@@ -19,7 +22,9 @@ struct Pull {
     state: Option<String>,
     title: Option<String>,
     body: Option<String>,
-    user: Option<User>
+    user: Option<User>,
+    merged_at: Option<String>,
+    created_at: Option<String>
 }
 
 #[derive(Deserialize, Debug)]
@@ -42,7 +47,7 @@ struct RateLimit {
 }
 
 #[tokio::main]
-async fn get_all_pulls(client: reqwest::Client, owner: &str,repo: &str) -> Result<Vec<Pull>,Error>{
+async fn get_all_pulls(client: &reqwest::Client, owner: &str, repo: &str) -> Result<Vec<Pull>,Error>{
     let mut request_url = format!("https://api.github.com/repos/{owner}/{repo}/pulls?state=all&per_page={PER_PAGE}");
     let pattern = Regex::new(r#"<([^>]+)>; rel="next""#).unwrap();
 
@@ -71,8 +76,44 @@ async fn get_all_pulls(client: reqwest::Client, owner: &str,repo: &str) -> Resul
     Ok(result)
 }
 
+fn get_users_pr_counts(client: &reqwest::Client, owner: &str, repo: &str, start_time: i64, end_time: i64) -> Result<HashMap<u32,User>,Error>{
+    let mut result = HashMap::new();
+    let pulls = get_all_pulls(client, owner, repo).unwrap();
+
+    for pull in pulls {
+        //ensure pull made by kwoc student
+        let create_time = DateTime::parse_from_rfc3339(pull.created_at.unwrap().as_str())
+            .unwrap()
+            .timestamp();
+
+        if (create_time < start_time) || (create_time > end_time) {
+            continue;
+        }
+
+        let key = pull.user.as_ref().unwrap().id.unwrap();
+
+        result.entry(key).or_insert({
+            let user ={
+                User {
+                    open_pr_count: Some(0),
+                    merged_pr_count: Some(0),
+                    ..pull.user.unwrap()
+                }
+            };
+            user
+        });
+        
+        if pull.state.unwrap() == "open" {
+            result.entry(key).and_modify(|i: &mut User| (*i).open_pr_count=Some((*i).open_pr_count.unwrap()+1));
+        }else if pull.merged_at.is_some() {
+            result.entry(key).and_modify(|i: &mut User| (*i).merged_pr_count=Some((*i).merged_pr_count.unwrap()+1));
+        }
+    }
+    Ok(result)
+}
+
 #[tokio::main]
-async fn print_rate_limit(client: reqwest::Client) -> Result<(),Error>{
+async fn print_rate_limit(client: &reqwest::Client) -> Result<(),Error>{
     let response = client.get("https://api.github.com/rate_limit")
         .send()
         .await?;
@@ -95,6 +136,10 @@ fn main() -> Result<(),Error> {
         }
     };
 
+    let KWOC_START_TIME = env::var("KWOC_START_TIME").expect("KWOC_START_TIME not found.").parse::<i64>().unwrap();
+    let KWOC_MID_EVALS_TIME = env::var("KWOC_MID_EVALS_TIME").expect("KWOC_MID_EVALS_TIME not found.").parse::<i64>().unwrap();
+    let KWOC_END_EVALS_TIME = env::var("KWOC_END_EVALS_TIME").expect("KWOC_END_EVALS_TIME not found.").parse::<i64>().unwrap();
+
     let client = match access_token {
         Some(token) => {
             reqwest::Client::builder()
@@ -116,8 +161,15 @@ fn main() -> Result<(),Error> {
         }
     };
 
-    print_rate_limit(client)?;
-    // let pulls = get_all_pulls(client, "kossiitkgp", "events")?;
+    print_rate_limit(&client)?;
+    let users = get_users_pr_counts(&client, "kossiitkgp", "events",KWOC_START_TIME,KWOC_MID_EVALS_TIME).unwrap();
+
+    for (key, value) in users.iter() {
+        println!("{key} {:?}",value);
+    }
+
+
+    // let pulls = get_all_pulls(&client, "kossiitkgp", "events")?;
     // println!("{:#?}", pulls);
     Ok(())
 }
