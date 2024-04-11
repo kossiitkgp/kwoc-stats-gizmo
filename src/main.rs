a0ec471bdf21ee884365b236d22accffd98f188e
Akash Mitra <am.akashmitra1000@gmail.com>
2024-04-11 15:37:14 +0530

    added access token support and querying rate limits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
-use reqwest::Error;
+use reqwest::{header, Error};
 use regex::Regex;
+use dotenv::dotenv;
+use std::env;
 
 const PER_PAGE: u32 = 50;
 
@@ -18,6 +20,25 @@ struct Pull {
     title: Option<String>,
     body: Option<String>,
     user: Option<User>
+}
+
+#[derive(Deserialize, Debug)]
+struct Rate {
+    limit: Option<u32>,
+    remaining: Option<u32>,
+    used: Option<u32>,
+    reset: Option<u32>
+}
+
+#[derive(Deserialize, Debug)]
+struct RateContainer {
+    core: Rate,
+    search: Rate
+}
+
+#[derive(Deserialize, Debug)]
+struct RateLimit {
+    resources: RateContainer
 }
 
 #[tokio::main]
@@ -50,12 +71,53 @@ async fn get_all_pulls(client: reqwest::Client, owner: &str,repo: &str) -> Resul
     Ok(result)
 }
 
-fn main() -> Result<(),Error> {       
-    let client = reqwest::Client::builder()
-        .user_agent("Rust")
-        .build()?;
+#[tokio::main]
+async fn print_rate_limit(client: reqwest::Client) -> Result<(),Error>{
+    let response = client.get("https://api.github.com/rate_limit")
+        .send()
+        .await?;
 
-    let pulls = get_all_pulls(client, "kossiitkgp", "events")?;
-    println!("{:#?}", pulls);
+    let rate_limit : RateLimit = response.json().await?;
+    println!("Core: {:#?}",rate_limit.resources.core);
+    println!("Search: {:#?}",rate_limit.resources.search);
+    Ok(())
+}
+
+fn main() -> Result<(),Error> {
+
+    dotenv().ok();
+
+    let access_token = match env::var("GH_ACCESS_TOKEN") {
+        Ok(token) => Some(token),
+        Err(err) => {
+            println!("GH_ACCESS_TOKEN not found.");
+            None
+        }
+    };
+
+    let client = match access_token {
+        Some(token) => {
+            reqwest::Client::builder()
+                .user_agent("Rust") 
+                .default_headers({
+                    let mut headers = reqwest::header::HeaderMap::new();
+                    headers.insert(
+                        reqwest::header::AUTHORIZATION,
+                        reqwest::header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap(),
+                    );
+                    headers
+                })
+                .build()?
+        }
+        None => {
+            reqwest::Client::builder()
+                .user_agent("Rust") 
+                .build()?
+        }
+    };
+
+    print_rate_limit(client)?;
+    // let pulls = get_all_pulls(client, "kossiitkgp", "events")?;
+    // println!("{:#?}", pulls);
     Ok(())
 }
